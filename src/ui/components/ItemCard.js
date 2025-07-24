c8f0b3838bdd589674e283447f2daa827115441d
Joaquin <joaquin@freightol.com>
2025-07-24 13:06:23 +0200

    awf

--- src/ui/components/ItemCard.js
+++ src/ui/components/ItemCard.js
@@ -5,6 +5,8 @@ import { sendToDesktop, downloadItem } from '../controllers/itemController.js';
 export function createItemCard(item) {
     const card = document.createElement('div');
     card.className = 'item-card';
+    // En ItemCard.js (dentro de createItemCard)
+    const favId = `favorite-${item.id}`; // Asegura ID único por card
 
     card.innerHTML = `
         <div class="item-picture">${item.picture}</div>
@@ -13,21 +15,16 @@ export function createItemCard(item) {
             <div class="item-description">${item.description}</div>
         </div>
         <div class="item-actions">
-            <button class="send-btn-modern">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" class="icon">
-                    <path fill="#000000"
-                        d="M14.2199 21.63C13.0399 21.63 11.3699 20.8 10.0499 16.83L9.32988 14.67L7.16988 13.95C3.20988 12.63 2.37988 10.96 2.37988 9.78001C2.37988 8.61001 3.20988 6.93001 7.16988 5.60001L15.6599 2.77001C17.7799 2.06001 19.5499 2.27001 20.6399 3.35001C21.7299 4.43001 21.9399 6.21001 21.2299 8.33001L18.3999 16.82C17.0699 20.8 15.3999 21.63 14.2199 21.63ZM7.63988 7.03001C4.85988 7.96001 3.86988 9.06001 3.86988 9.78001C3.86988 10.5 4.85988 11.6 7.63988 12.52L10.1599 13.36C10.3799 13.43 10.5599 13.61 10.6299 13.83L11.4699 16.35C12.3899 19.13 13.4999 20.12 14.2199 20.12C14.9399 20.12 16.0399 19.13 16.9699 16.35L19.7999 7.86001C20.3099 6.32001 20.2199 5.06001 19.5699 4.41001C18.9199 3.76001 17.6599 3.68001 16.1299 4.19001L7.63988 7.03001Z" />
-                    <path fill="#000000"
-                        d="M10.11 14.4C9.92 14.4 9.73 14.33 9.58 14.18C9.29 13.89 9.29 13.41 9.58 13.12L13.16 9.53C13.45 9.24 13.93 9.24 14.22 9.53C14.51 9.82 14.51 10.3 14.22 10.59L10.64 14.18C10.5 14.33 10.3 14.4 10.11 14.4Z" />
-                </svg>
-                <p class="text">
-                    <span>S</span><span>e</span><span>n</span><span>d</span>
-                    <span class="tab"></span>
-                    <span>D</span><span>e</span><span>s</span><span>k</span>
-                </p>
-            </button>
-
+            <input type="checkbox" id="${favId}" class="fav-btn-checkbox">
+            <label for="${favId}" class="fav-btn-label" style="margin-right:10px; cursor:pointer;">
+                <span class="fav-btn-icon" style="display:inline-block;">
+                    <svg class="fav-btn-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -2 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 2C2.44772 2 2 2.44772 2 3V14C2 14.5523 2.44772 15 3 15H21C21.5523 15 22 14.5523 22 14V3C22 2.44772 21.5523 2 21 2H3zM3 0H21C22.6569 0 24 1.34315 24 3V14C24 15.6569 22.6569 17 21 17H3C1.34315 17 0 15.6569 0 14V3C0 1.34315 1.34315 0 3 0zM16 18C16.5523 18 17 18.4477 17 19C17 19.5523 16.5523 20 16 20H8C7.44772 20 7 19.5523 7 19C7 18.4477 7.44772 18 8 18H16z" fill="#ffffffff"/></svg>
+                </span>
+                <div class="fav-btn-action">
+                    <span class="fav-btn-option-1">Send to desktop</span>
+                    <span class="fav-btn-option-2">Remove to desktop</span>
+                </div>
+            </label>
             <button class="Btn" data-id="${item.id}">
                 <svg class="svgIcon" viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg">
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5
@@ -39,11 +36,37 @@ export function createItemCard(item) {
             </button>
         </div>
     `;
+    const favLabel = card.querySelector('.fav-btn-label');
+    const favIconSpan = favLabel.querySelector('.fav-btn-icon');
+    let isFav = false;
 
-    const sendBtn = card.querySelector('.send-btn-modern');
+    const svgOn = `<svg class="fav-btn-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -2 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 2C2.44772 2 2 2.44772 2 3V14C2 14.5523 2.44772 15 3 15H21C21.5523 15 22 14.5523 22 14V3C22 2.44772 21.5523 2 21 2H3zM3 0H21C22.6569 0 24 1.34315 24 3V14C24 15.6569 22.6569 17 21 17H3C1.34315 17 0 15.6569 0 14V3C0 1.34315 1.34315 0 3 0zM16 18C16.5523 18 17 18.4477 17 19C17 19.5523 16.5523 20 16 20H8C7.44772 20 7 19.5523 7 19C7 18.4477 7.44772 18 8 18H16z" fill="#ffffffff"/></svg>`;
+    const svgOff = `<svg class="fav-btn-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.7071 1.70711C24.0976 1.31658 24.0976 0.683417 23.7071 0.292893C23.3166 -0.0976311 22.6834 -0.0976311 22.2929 0.292893L21.2933 1.29253C21.2934 1.29241 21.2931 1.29265 21.2933 1.29253L0.292893 22.2929C-0.0976311 22.6834 -0.0976311 23.3166 0.292893 23.7071C0.683417 24.0976 1.31658 24.0976 1.70711 23.7071L2.70669 22.7075C2.70655 22.7077 2.70683 22.7074 2.70669 22.7075L23.7071 1.70711Z" fill="#ffffffff"/><path d="M17.7574 2H3C1.34315 2 1.49012e-08 3.34314 1.49012e-08 5V16C1.49012e-08 17.0219 0.510917 17.9244 1.29124 18.4661L2.78134 16.976C2.33422 16.8763 2 16.4772 2 16V5C2 4.44771 2.44772 4 3 4H15.7574L17.7574 2Z" fill="#ffffffff"/><path d="M22 6.24264L23.1213 5.12132C23.4489 4.79378 23.6848 4.40934 23.8292 3.99982C23.9398 4.31265 24 4.64929 24 5V16C24 17.6569 22.6569 19 21 19H9.24264L11.2426 17H21C21.5523 17 22 16.5523 22 16V6.24264Z" fill="#ffffffff"/><path d="M7.02398 21.2187L8.24264 20H16C16.5523 20 17 20.4477 17 21C17 21.5523 16.5523 22 16 22H8C7.52282 22 7.12371 21.6658 7.02398 21.2187Z" fill="#ffffffff"/></svg>`;
+
+    favLabel.addEventListener('click', (e) => {
+        e.preventDefault();
+        isFav = !isFav;
+        favIconSpan.innerHTML = isFav ? svgOff : svgOn;
+
+        if (isFav) {
+            sendToDesktop(favLabel, item.id);
+        } else {
+            // await removeItemFromOwnedList(item.id);
+        }
+    });
+
+
     const downloadBtn = card.querySelector('.Btn');
+    const favoriteInput = card.querySelector('.fav-btn-checkbox');
+    // favoriteInput.addEventListener('change', (e) => {
+    //     console.log("afjnkaw")
+    //     if (e.target.checked) {
+    //         debugger;
+    //         sendToDesktop(favoriteInput, item.id); // O tu lógica de favorito
+    //     }
+    //     // Si quieres manejar des-favorito, añade lógica aquí
+    // });
 
-    sendBtn.addEventListener('click', () => sendToDesktop(sendBtn, item.id));
     downloadBtn.addEventListener('click', () => downloadItem(item.id));
 
     return card;
